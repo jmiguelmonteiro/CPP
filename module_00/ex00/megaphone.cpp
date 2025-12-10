8769f47f5f0aa4a4aa1f19dc28962214d30f348e
josemigu <josemigu@student.42porto.com>
2025-12-10 14:48:26 +0000

    module00, ex01

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,19 +6,27 @@
 /*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 18:47:54 by josemigu          #+#    #+#             */
-/*   Updated: 2025/12/08 18:53:14 by josemigu         ###   ########.fr       */
+/*   Updated: 2025/12/10 14:38:15 by josemigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <fstream>
+#include <cstdlib>
 
-int main(int argc, char **argv)
-{
+char my_toupper(char ch) {
+    return static_cast<char>(std::toupper(static_cast<unsigned char>(ch)));
+}
+
+int main(int argc, char **argv) {
 	(void)argv;
 	
     if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return 1;
+    } else {
+        for (int i = 1; i < argc; i++)
+            for (int j = 0; argv[i][j]; j++)
+                std::cout << my_toupper(argv[i][j]);
+        std::cout << std::endl;
     }
+    return (EXIT_SUCCESS);
 }
---
+++ module_00/ex01/PhoneBook.class.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/10 14:41:28 by josemigu          #+#    #+#             */
+/*   Updated: 2025/12/10 14:45:56 by josemigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+class PhoneBook {
+
+private:
+
+public:
+
+	PhoneBook( void );
+	~PhoneBook( void );
+	
+};
+
+#endif
