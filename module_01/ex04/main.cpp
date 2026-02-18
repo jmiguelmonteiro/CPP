a161f1f426897119243cb437d755974341a331d1
josemigu <josemigu@student.42porto.com>
2026-02-18 17:01:25 +0000

    CPP01: ex04

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -12,25 +12,58 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <cstdlib>
+
+void	replace(std::ifstream &inFile, std::ofstream &outFile, const std::string s1, const std::string s2) {
+
+	std::string	nextLine;
+
+	while (getline(inFile, nextLine)) {
+		std::size_t pos = 0;
+		while ((pos = nextLine.find(s1, pos)) != std::string::npos) {
+			nextLine.erase(pos, s1.length());
+			nextLine.insert(pos, s2);
+			pos += s2.length();
+		}
+		outFile << nextLine << std::endl;		
+	}
+}
 
 int main(int argc, char **argv) {
 
-	(void)argv;
 	if (argc != 4) {
-		std::cout << "Usage: ./a.out {filename} {s1} {s2}\n";
+		std::cerr << "Usage: ./a.out {filename} {s1} {s2}\n";
 		return (EXIT_FAILURE);
 	}
 	
+	std::string	inFileName = argv[1];
+	std::string	outFileName = inFileName + ".replace";
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 
-	// if (s1.empty() || s2.empty()) {
-	// 	std::cout << "either {s1} or {s2} are empty\n";
-	// 	return (EXIT_FAILURE);
-	// }
+	if (s1.empty()) {
+		std::cerr << "{s1} cannot be empty\n";
+		return (EXIT_FAILURE);
+	}
 
+	std::ifstream	inFile;
+	inFile.open(inFileName.c_str());
+	if (!inFile.is_open()) {
+		std::cerr << "Cannot open input file: " << inFileName << std::endl;
+		return (EXIT_FAILURE);
+	}
 
+	std::ofstream	outFile;
+	outFile.open(outFileName.c_str());
+	if (!outFile.is_open()) {
+		std::cerr << "Cannot open output file: " << outFileName << std::endl;
+		return (EXIT_FAILURE);
+	}
 	
+	replace(inFile, outFile, s1, s2);
+
+	outFile.close();
+	inFile.close();
     return EXIT_SUCCESS;
 }
